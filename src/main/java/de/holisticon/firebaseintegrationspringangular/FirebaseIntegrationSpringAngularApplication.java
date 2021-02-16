137c1c8e79df8b814fc976c945be7cd4b8a73960
Timo Stübing <timo.stuebing@holisticon.de>
2021-02-16 18:15:14 +0100

    feat: Initial commit

---
+++ src/main/java/de/holisticon/firebaseintegrationspringangular/FirebaseIntegrationSpringAngularApplication.java
@@ -0,0 +1,13 @@
+package de.holisticon.firebaseintegrationspringangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirebaseIntegrationSpringAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirebaseIntegrationSpringAngularApplication.class, args);
+	}
+
+}
